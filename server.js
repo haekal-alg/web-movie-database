4625eba7eac52dace69f9a78e61fdb5452fc7b5f
Alvitoikrm <85780874+Alvitoikrm@users.noreply.github.com>
2022-05-26 23:36:56 +0700

    fix login and register auth

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ require("dotenv").config();
 const express = require("express");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
+const bcrypt = require("bcrypt");
 const app = express();
 const { Client } = require("pg");
 const bp = require("body-parser");
@@ -28,111 +29,122 @@ const db = new Client({
 */
 
 const db = new Client({
-    host: "localhost",
-    user: "postgres",
-    database: "web_movie_database",
-    password: "haekal",
-    port: 5432,
+  host: "localhost",
+  user: "postgres",
+  database: "final",
+  port: 5432,
 });
 
 // session middleware
 const oneDay = 1000 * 60 * 60 * 24;
-    app.use(
-        session({
-            secret: "this is a secret",
-            saveUninitialized: true,
-            cookie: { maxAge: oneDay },
-            resave: false,
-    })
+app.use(
+  session({
+    secret: "this is a secret",
+    saveUninitialized: true,
+    cookie: { maxAge: oneDay },
+    resave: false,
+  })
 );
 
 app.get("/", (req, res) => {
-    // if user is not logged in then send it to the login page
-    if (!req.session.userid) res.redirect("../login.html");
-    else res.redirect("../search.html");
+  // if user is not logged in then send it to the login page
+  if (!req.session.userid) res.redirect("../login.html");
+  else res.redirect("../search.html");
 });
 
 app.get("/api/get_session", (req, res) => {
-    res.send(req.session);
+  res.send(req.session);
 });
 
-app.post("/api/authenticate", (req, res) => {
-    const { username, password } = req.body;
-    /*
-    const query = `SELECT password FROM users WHERE username = '${username};'`;
-   
-    db.query(query, (err, results) => {
-        if (err){
+app.post("/api/login", async (req, res) => {
+  const { username, password } = req.body;
+  const query = `SELECT password FROM users WHERE username = '${username};'`;
+  db.query(query, (err, results) => {
+    if (err) {
+      console.log(err);
+      alert("Login failed");
+      return;
+    }
+    try {
+      //if admin, redirect to admin.html
+      const dataPassword = JSON.parse(JSON.stringify(results));
+      if (bcrypt.compare(password, dataPassword)) {
+        if (username != "Admin") {
+          let user_session = req.session;
+          user_session.userid = username;
+          console.log(`Login as ${user_session.userid}`);
+          // if successful, redirect to search.html
+          res.redirect("../search.html");
+        }
+        res.redirect("../admin.html");
+      } else {
+        res.send("Invalid username or password");
+      }
+    } catch {
+      res.status(500).send();
+    }
+  });
+
+  // hanya untuk testing
+  // if (username == 'user1' && password == 'user1'){
+  //     let user_session = req.session;
+  //     user_session.userid = username;
+  //     res.redirect("../search.html")
+  // } else if (username == 'admin' && password == 'admin'){
+  //     let user_session = req.session;
+  //     user_session.userid = username;
+  //     res.redirect("../admin.html")
+  // }
+  // else {
+  //     res.send('Invalid username or password');
+  // }
+});
+
+app.post("/register", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  const repassword = req.body.repassword;
+
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+    //kondisi apabila username sama dengan database belum
+    if (!username || !password || !repassword) {
+      alert("Enter all fields!");
+      res.send("Blank field");
+    }
+    if (password != repassword) {
+      alert("Password doesn't match!");
+      res.send("password doesn't match");
+    } else {
+      //form passed
+      db.query(
+        "INSERT INTO users (username, password) VALUES ($1, $2)",
+        [username, hashedPassword],
+        (err) => {
+          if (err) {
             console.log(err);
-            alert('Login failed');
             return;
+          }
+          res.send("Register Successful");
+          res.redirect("../login.html");
         }
-        try {
-            // if successful, redirect to search.html
-            if(await bcrypt.compare(password, results.rows)) {
-                let user_session = req.session;
-                user_session.userid = username;
-                console.log(`Login as ${user_session.userid}`);
-                res.redirect("../search.html")
-            } else{
-                res.send("Invalid username or password");            
-            }
-        }
-        catch {
-            res.status(500).send();
-        }
-    });
-    */
-
-    // hanya untuk testing
-    if (username == 'user1' && password == 'user1'){
-        let user_session = req.session;
-        user_session.userid = username;
-        res.redirect("../search.html")
-    } else if (username == 'admin' && password == 'admin'){
-        let user_session = req.session;
-        user_session.userid = username;
-        res.redirect("../admin.html")
+      );
     }
-    else {
-        res.send('Invalid username or password');
-    }
+  } catch {
+    res.status(500).send();
+  }
 });
-
-/* gw comment dulu soalnya masih ada error
-app.post("/api/register", (req, res) => {
-    // Belum bikin jika username sama kayak di database 
-    try{
-        const hashedPassword = await bycrpt.hash(req.body.password, 10);
-        const username = req.body.username;
-        db.query(
-            "INSERT INTO users (username, password) VALUES ($1, $2)",
-            [username, hashedPassword],
-            (err) => {
-              if (err) {
-                console.log(err);
-                return;
-              }
-              res.send("Register Successful");
-            }
-          );
-
-    } catch{
-        res.status(500).send();
-    }
-});
-*/
 
 app.get("/api/logout", (req, res) => {
-    req.session.destroy();
-    res.redirect("../login.html");
+  req.session.destroy();
+  res.redirect("../login.html");
 });
 
-app.post('/api/search_from_user', (req, res) => {
-    const { title, username } = req.body
-    console.log(username)
-    const query = 
-    `SELECT 
+app.post("/api/search_from_user", (req, res) => {
+  const { title, username } = req.body;
+  console.log(username);
+  const query = `SELECT 
         m.title, 
         TO_CHAR(m.release_date, 'dd Month yyyy'), 
         m.runtime, 
@@ -160,23 +172,22 @@ app.post('/api/search_from_user', (req, res) => {
     LEFT JOIN
         (SELECT * FROM users_mov_statuses WHERE username = '${username}') AS s 
         ON s.movie_id = m.movie_id
-    WHERE lower(m.title) SIMILAR TO '(${title}%|%${title}%|%${title})';` // at the start, middle or end
+    WHERE lower(m.title) SIMILAR TO '(${title}%|%${title}%|%${title})';`; // at the start, middle or end
 
-    db.query(query, (err, results) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.log(results.rows)
-        res.status(200).send(results.rows);
-    });
-})
+  db.query(query, (err, results) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(results.rows);
+    res.status(200).send(results.rows);
+  });
+});
 
-app.post('/api/search_from_admin', (req, res) => {
-    const { title } = req.body
+app.post("/api/search_from_admin", (req, res) => {
+  const { title } = req.body;
 
-    const query = 
-    `SELECT 
+  const query = `SELECT 
         m.movie_id,
         m.title, 
         TO_CHAR(m.release_date, 'dd Month yyyy'), 
@@ -197,62 +208,60 @@ app.post('/api/search_from_admin', (req, res) => {
     LEFT JOIN
         (SELECT movie_id, COUNT(movie_id) FROM users_ratings GROUP BY movie_id) AS v
         ON v.movie_id = m.movie_id
-    WHERE lower(m.title) SIMILAR TO '(${title}%|%${title}%|%${title})';` // at the start, middle or end
+    WHERE lower(m.title) SIMILAR TO '(${title}%|%${title}%|%${title})';`; // at the start, middle or end
 
-    db.query(query, (err, results) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.log(results.rows);
-        res.status(200).send(results.rows);
-    });
+  db.query(query, (err, results) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log(results.rows);
+    res.status(200).send(results.rows);
+  });
 });
 
-app.post('/api/update_status', (req, res) => {
-    const jsonArray = req.body;
-    console.log(jsonArray)
-    var query = '';
+app.post("/api/update_status", (req, res) => {
+  const jsonArray = req.body;
+  console.log(jsonArray);
+  var query = "";
 
-    // form the query
-    for (var i = 0; i < jsonArray.length; i++) {
-        //console.log(jsonArray[i])
-        var { username, status, title } = jsonArray[i];
-        // delete row if status null/radio button is both unchecked
-        if (status == null) {
-            query += 
-            `DELETE FROM users_mov_statuses 
+  // form the query
+  for (var i = 0; i < jsonArray.length; i++) {
+    //console.log(jsonArray[i])
+    var { username, status, title } = jsonArray[i];
+    // delete row if status null/radio button is both unchecked
+    if (status == null) {
+      query += `DELETE FROM users_mov_statuses 
             WHERE username = '${username}' AND movie_id = (SELECT movie_id FROM movies WHERE title = '${title}');`;
-        }
-        // otherwise insert/update status
-        else {
-            query += 
-            `INSERT INTO users_mov_statuses (username, status, movie_id) VALUES 
+    }
+    // otherwise insert/update status
+    else {
+      query += `INSERT INTO users_mov_statuses (username, status, movie_id) VALUES 
                 ('${username}', ${status}, (SELECT movie_id FROM movies WHERE title = '${title}')) 
                 ON CONFLICT (username, movie_id) DO UPDATE SET status = ${status};
             `;
-        }
     }
-    //console.log(query)
-    db.query(query, (err, results) => {
-        if (err) {
-            console.log(err);
-            return;
-        }
-        console.log('Status is Updated!')
-        //console.log(results.rows);
-        //res.status(200).send(results.rows);
-    });
-})
+  }
+  //console.log(query)
+  db.query(query, (err, results) => {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    console.log("Status is Updated!");
+    //console.log(results.rows);
+    //res.status(200).send(results.rows);
+  });
+});
 
 db.connect((err) => {
-    if (err) {
-        console.log(err);
-        return;
-    }
-    console.log(`Connected to ${process.env.DB_DATABASE}`);
+  if (err) {
+    console.log(err);
+    return;
+  }
+  console.log(`Connected to ${process.env.DB_DATABASE}`);
 });
 
 app.listen(8080, () => {
-    console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
